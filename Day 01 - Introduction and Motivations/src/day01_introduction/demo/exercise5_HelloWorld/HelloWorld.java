24bd108b18e9f050cf8548f3cf049dbd82f83f72
tbaodang <baorox5@gmail.com>
2024-08-28 20:30:02 +0700

    first commit

---
+++ Day 01 - Introduction and Motivations/src/day01_introduction/demo/exercise5_HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package day01_introduction.demo.exercise5_HelloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// Print out "Hello, World"
+		System.out.println("Hello, World!"); //
+	}
+}
